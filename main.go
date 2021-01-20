2fb43a7f0aa20e2d5419211b7ea6896cc6abf37c
Michael Zimmermann <michael@ml-zn.de>
2021-01-20 22:34:15 +0100

    tests

--- main.go
+++ main.go
@@ -3,22 +3,23 @@ package main
 import (
   "fmt"
   "github.com/alecthomas/kong"
+  dCli "github.com/DigitalVault/diva/cli"
 )
 
 var CLI struct {
   Scan struct {
     Path string `help:"Path to the digital vault location." default:"."`
 
-  } `cmd help:"Scan filesystem for files to be preserved."`
+  } `cmd:"" help:"Scan filesystem for files to be preserved."`
 
   Ingest struct {
     Path string `help:"Path to the digital vault location." default:"."`
 
-  } `cmd help:"Scan filesystem and ingest files to the digital vault."`
+  } `cmd:"" help:"Scan filesystem and ingest files to the digital vault."`
 
   Version struct {
 
-  } `cmd help:"Prints version info of the diva-cli"`
+  } `cmd:"" help:"Prints version info of the diva-cli"`
 }
 
 func main() {
@@ -28,6 +29,8 @@ func main() {
     fmt.Println("Scan :", ctx.Args)
   case "ingest":
     fmt.Println("Ingest :", ctx.Args)
+  case "version":
+    fmt.Println("Version :", dCli.Version())
   default:
     panic(ctx.Command())
 
