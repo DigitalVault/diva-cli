963019366bab46da28ad42cc96fbc63f7a60d200
Michael Zimmermann <michael@ml-zn.de>
2021-01-20 18:32:52 +0100

    additional cli args

--- main.go
+++ main.go
@@ -7,21 +7,27 @@ import (
 
 var CLI struct {
   Scan struct {
+    Path string `help:"Path to the digital vault location." default:"."`
 
   } `cmd help:"Scan filesystem for files to be preserved."`
 
   Ingest struct {
+    Path string `help:"Path to the digital vault location." default:"."`
 
   } `cmd help:"Scan filesystem and ingest files to the digital vault."`
+
+  Version struct {
+
+  } `cmd help:"Prints version info of the diva-cli"`
 }
 
 func main() {
   ctx := kong.Parse(&CLI)
   switch ctx.Command() {
   case "scan":
-    fmt.Println("Scan")
+    fmt.Println("Scan :", ctx.Args)
   case "ingest":
-    fmt.Println("Ingest")
+    fmt.Println("Ingest :", ctx.Args)
   default:
     panic(ctx.Command())
 
